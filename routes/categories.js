981d69c147d19e0f178712a45b4c83abb25881ea
Ayoub Nejjari <raysoflee07@gmail.com>
2017-05-25 12:32:05 +0000

    sorting by category logic

--- routes/categories.js
+++ routes/categories.js
@@ -8,16 +8,32 @@ var db = mongoose.connection;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  db.collection('categories').find({}, {_id:0}).toArray(function(err, cats) {
+  db.collection('categories').find().toArray(function(err, cats) {
     if (err) {
       console.log(err);
     } else {
-        console.log(cats);
         res.render('categories', { title: 'Gategories', desc: 'Choose your category', cats: cats });
 
     }
   });
 //  res.render('categories', { title: 'Gategories', desc: 'Choose your category' });
 });
+
+
+router.get('/:id', function(req, res, next) {
+  db.collection('categories').find().toArray(function(err, cats) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(req.params.id);
+      var title = req.params.id.toUpperCase() + ' Articles'
+        res.render('articles', { title: title,  desc: 'Articles sorted by category', cats: cats });
+
+    }
+  });
+//  res.render('categories', { title: 'Gategories', desc: 'Choose your category' });
+});
+
+
 
 module.exports = router;
